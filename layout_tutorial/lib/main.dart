a71a75d48bd52283d303dda976120b6fb012bbd4
seiyasugioka <sugiokaseiya@team-lab.com>
2019-04-27 20:48:42 +0900

    お気に入り機能追加

--- layout_tutorial/lib/main.dart
+++ layout_tutorial/lib/main.dart
@@ -34,11 +34,7 @@ class MyApp extends StatelessWidget {
                 ],
               )
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -111,5 +107,52 @@ class MyApp extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  //無名関数の記述方法
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+//FavoriteWidgetに使用されるStateだと宣言している？
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              color: Colors.red[500],
+              onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }
